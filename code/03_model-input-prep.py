fe52043ffd8f893daad17a1a82a6785d872528a4
ksbuffaloe <ksbuffaloe@gmail.com>
2024-11-08 15:46:20 -0700

    changed train test split naming conventions and add a line to store class distribution for exporting

--- code/03_model-input-prep.py
+++ code/03_model-input-prep.py
@@ -15,20 +15,29 @@ train_indices = indices_df['train_indices'].dropna().astype(int).tolist()
 test_indices = indices_df['test_indices'].dropna().astype(int).tolist()
 
 # Load a cleaned/preprocessed dataset (e.g., with or without stop words)
-df_cleaned = pd.read_csv('cleaned data version you need.csv')
+df_cleaned = pd.read_csv(r'data/cleaned_data/cleaned data version you need.csv')
 
 # Filter labeled data for the specified values
 df_labeled = df_cleaned[df_cleaned['Target_Col'].isin(['Var1', 'Var2'])]
 
-# Change target class variables to 0 and 1 for model input
+#print and store our our class distribution
+print(f'Class distribution: \n{df_labeled['Target_Col'].value_coutns()}')
+
+# Get value counts
+value_counts = df_labeled['Target_Col'].value_counts()
+
+# format our value counts that will then be used later to export
+formatted_counts = '\n'.join([f"{cls}: {count}" for cls, count in value_counts.items()])
+
+# Change target class variables to 0 and 1 for model input assing 1 to the positive class you want
 df_labeled['Target_Col'] = df_labeled['Target_Col'].replace({'Var1': 0, 'Var2': 1})
 
 X = df_labeled['Text_Col']
 y = df_labeled['Target_Col']
 
 # Apply the saved indices to split the data
-X_train, X_test = X.loc[train_indices], X.loc[test_indices]
-y_train, y_test = y.loc[train_indices], y.loc[test_indices]
+X_train_raw, X_test_raw = X.loc[train_indices], X.loc[test_indices]
+y_train_raw, y_test_raw = y.loc[train_indices], y.loc[test_indices]
 
 #_______________________________________________________________________________________________
 #BELOW IS THE CODE TO USE A TFIDF METHOD TO BE USED AS MODEL INPUT, USE THIS CODE BLOCK TO IMPLEMENT
@@ -38,8 +47,8 @@ y_train, y_test = y.loc[train_indices], y.loc[test_indices]
 tfidf_vec = TfidfVectorizer(min_df = 2 , max_df = .8, ngram_range=(1,2))
 
 #transform our X train/test split into vectors for analysis later
-X_train = tfidf_vec.fit_transform(X_train)
-X_test  = tfidf_vec.transform(X_test)
+X_train = tfidf_vec.fit_transform(X_train_raw)
+X_test  = tfidf_vec.transform(X_test_raw)
 
 #verify that the vector transformation was succesful
 print(f'Number of vector features in training split: {X_train.shape}')
@@ -54,8 +63,8 @@ print(f'Number of vector features in testing split: {X_test.shape}')
 count_vec = CountVectorizer(min_df = 2 , max_df = .8, ngram_range=(1,2))
 
 #transform our X train/test split into vectors for analysis later
-X_train = count_vec.fit_transform(X_train)
-X_test  = count_vec.transform(X_test)
+X_train = count_vec.fit_transform(X_train_raw)
+X_test  = count_vec.transform(X_test_raw)
 
 #verify that the vector transformation was succesful
 print(f'Number of vector features in training split: {X_train.shape}')
@@ -66,7 +75,7 @@ print(f'Number of vector features in testing split: {X_test.shape}')
 #BELOW IS THE CODE TO USE WORD EMBEDDINGS AS THE INPUT INTO THE MODEL, USE THIS CODE BLOCK TO IMPLEMENT
 #_______________________________________________________________________________________________
  
- #-----------------------------------------------------------
+#-----------------------------------------------------------
 #these first 5 steps are to get the embeddings and add them to our complete cleaned dataframe, once they are run comment them out to avoid running again
 
 #Step 1: bring in model from HuggingFace
@@ -81,13 +90,13 @@ embeddings_df = pd.DataFrame(embeddings, columns = [f'embedding_{i}' for i in ra
 #Step 4: Add the embeddings to our cleaned dataframe as new columns
 df_cleaned_embed = pd.concat([df_cleaned, embeddings_df], axis = 1 )
 
-#Step 5: Export the dataframe with the embeddings 
-df_cleaned_embed.to_csv("INSERT NEW EMBEDDING FILE PATH HERE.CSV", index = False)
+#Step 5: Export the dataframe with the embeddings BE SURE TO SPECIFY THE TYPE (with or without stopwords ect.)
+df_cleaned_embed.to_csv(r'data/embeddings/INSERT NEW EMBEDDING FILE PATH NAME HERE.CSV', index = False)
 
 #----------------------------------------------------------
 
 #load in the embedings dataframe if the file exists already. Else run the steps above to create the new file. Comment this code out if not needed
-df_cleaned_embed = pd.read_csv('INSERT EMBEDDINGS FILE PATH.CSV')
+df_cleaned_embed = pd.read_csv(r'data/embeddings/INSERT RELAVENT EMBEDDINGS FILE PATH.CSV')
 
 #create our labeled embeddings dataframe
 df_labeled_embed = df_cleaned_embed[df_cleaned_embed['Target_Col'].isin(['Var1', 'Var2'])]
@@ -97,12 +106,12 @@ embeddings = df_labeled_embed[[col for col in df_labeled_embed.columns if col.st
 labels = df_labeled_embed['Target_col']
 
 # Apply the saved indices to split the data for our embeddings dataframe
-X_train, X_test = embeddings.loc[train_indices], embeddings.loc[test_indices]
-y_train, y_test = labels.loc[train_indices], labels.loc[test_indices]
+X_train_raw_embed, X_test_raw_embed = embeddings.loc[train_indices], embeddings.loc[test_indices]
+y_train_raw_embed, y_test_raw_embed = labels.loc[train_indices], labels.loc[test_indices]
 
 # If the embeddings are in a compatible format (like lists or numpy arrays), you can directly fit a model
 # Convert to numpy arrays if necessary
-X_train = np.array(X_train)
-X_test = np.array(X_test)
+X_train = np.array(X_train_raw_embed)
+X_test = np.array(X_test_raw_embed)
 
 
